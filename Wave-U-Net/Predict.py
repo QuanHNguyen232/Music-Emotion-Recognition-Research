60759e667947d7c29b8d4c849468d3db49f92079
Viet Nguyen <alexvn.edu@gmail.com>
2022-07-22 10:58:51 +0700

    add code

--- Wave-U-Net/Predict.py
+++ Wave-U-Net/Predict.py
@@ -11,7 +11,16 @@ def cfg():
     input_path = os.path.join("audio_examples", "The Mountaineering Club - Mallory", "mix.mp3") # Which audio file to separate
     output_path = None # Where to save results. Default: Same location as input.
 
+# @ex.automain
+# def main(cfg, model_path, input_path, output_path):
+#     model_config = cfg["model_config"]
+#     Evaluate.produce_source_estimates(model_config, model_path, input_path, output_path)
+
 @ex.automain
 def main(cfg, model_path, input_path, output_path):
     model_config = cfg["model_config"]
-    Evaluate.produce_source_estimates(model_config, model_path, input_path, output_path)
+    for input_name in os.listdir(input_path):
+        _input_path = os.path.join(input_path, input_name)
+        _output_path = os.path.join(output_path, input_name)
+        os.makedirs(_output_path)
+        Evaluate.produce_source_estimates(model_config, model_path, _input_path, _output_path)
